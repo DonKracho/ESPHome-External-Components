301755bd56bdeac332dca6dfb1c3010bf276f291
Wolfgang Kracht <DonKracho@users.noreply.github.com>
2024-05-10 15:59:22 +0200

    cleanup rf_switch.cpp

--- components/rf_switch/rf_switch.cpp
+++ components/rf_switch/rf_switch.cpp
@@ -7,19 +7,20 @@ namespace rf_switch {
 static const char *TAG = "rf_switch.switch";
 
 void RfSwitch::write_state(bool state) {
-    // This will be called every time the user requests a state change.
-	
-	float value = (state) ? 0.5f : 0.0f;
-    output_->set_level(value + (0.001f * channel_));
+  // This will be called every time the user requests a state change.
+  // ESPHome float_output does allow states between 0.0 and 1.0 only.
+  // Do some magic to code channel and state into a float value
+  float value = (state) ? 0.5f : 0.0f;
+  output_->set_level(value + (0.001f * channel_));
 
-    // Acknowledge new state by publishing it
-    publish_state(state);
+  // optimistic acknowledge of new state by publishing it
+  publish_state(state);
 }
 
 void RfSwitch::dump_config() {
-    ESP_LOGCONFIG(TAG, "RF custom switch");
-	// LOG_BINARY_OUTPUT(output_); //can't be used because it accesses protected members.
-	ESP_LOGCONFIG(TAG, "  channel: %d", channel_);
+  ESP_LOGCONFIG(TAG, "RF custom switch");
+  // LOG_BINARY_OUTPUT(output_); //can't be used because it accesses protected members.
+  ESP_LOGCONFIG(TAG, "  channel: %d", channel_);
 }
 
 } //namespace rf_switch
