606fc1f0af8e490d9bc497b07450f9335f22e86f
Wolfgang Kracht <DonKracho@users.noreply.github.com>
2024-05-12 16:42:43 +0200

    Support of groups for rf_switch.h

--- components/rf_switch/rf_switch.h
+++ components/rf_switch/rf_switch.h
@@ -14,13 +14,13 @@ class RfSwitch : public switch_::Switch, public Component {
   void dump_config() override;
   
   float get_setup_priority() const { return setup_priority::HARDWARE; }
-  //void set_rf_pin(InternalGPIOPin *pin) { rfpin_ = pin; }
   void set_output(output::FloatOutput *output) { output_ = output; }
+  void set_group(uint16_t group) { group_ = group; }
   void set_channel(uint16_t channel) { channel_ = channel; }
 
  protected:
   output::FloatOutput *output_{nullptr};
-  //InternalGPIOPin *rfpin_{nullptr};
+  uint16_t group_{0};
   uint16_t channel_{0};
 };
 
