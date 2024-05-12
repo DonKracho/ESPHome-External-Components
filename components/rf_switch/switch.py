bf792254ca3c37a0d0b8b087bfdb9d788668f183
Wolfgang Kracht <DonKracho@users.noreply.github.com>
2024-05-12 16:40:09 +0200

    support of groups switch.py

--- components/rf_switch/switch.py
+++ components/rf_switch/switch.py
@@ -13,13 +13,14 @@ DEPENDENCIES = []
 AUTO_LOAD = ['switch']
 
 CONF_RFPIN = "rf_pin"
+CONF_GROUP = "group"
 CONF_CHANNEL = "channel"
 
 CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(RfSwitch),
     cv.Required(CONF_OUTPUT): cv.use_id(output.FloatOutput),
-#    cv.Required(CONF_RFPIN): pins.gpio_output_pin_schema,
     cv.Required(CONF_CHANNEL): vol.All(cv.uint16_t, vol.Range(min=0, max=15)),
+    cv.Optional(CONF_GROUP, Default=0): vol.All(cv.uint16_t, vol.Range(min=0, max=1)),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -29,7 +30,5 @@ async def to_code(config):
     out = await cg.get_variable(config[CONF_OUTPUT])
     cg.add(var.set_output(out))
 
-#    p = await cg.gpio_pin_expression(config[CONF_RFPIN])
-#    cg.add(var.set_rf_pin(p))
-    
     cg.add(var.set_channel(config[CONF_CHANNEL]))
+    cg.add(var.set_group(config[CONF_GROUP]))
